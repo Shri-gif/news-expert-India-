85bfab8ffe6206dca46715bb5f3c4805607bed8b
Shri-gif <271024975+Shri-gif@users.noreply.github.com>
2026-04-07 11:29:21 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,1 +1,15 @@
+import os
+from datetime import date
 
+class Config:
+    EMAIL_TO = os.getenv('EMAIL_TO')
+    EMAIL_FROM = os.getenv('EMAIL_FROM') 
+    EMAIL_PASSWORD = os.getenv('EMAIL_PASS')
+    
+    NEWS_SOURCES = {
+        'Economic Times': 'https://economictimes.indiatimes.com/',
+        'The Hindu': 'https://www.thehindu.com/',
+        'InShorts': 'https://www.inshorts.com/'
+    }
+    
+    TODAY = date.today().strftime('%d/%m/%Y')
